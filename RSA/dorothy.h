7d04ad8764e5f1778a7df7206fa94d4cf5ed8a78
gsgaytan <gsgaytan@tamu.edu>
2015-04-16 14:37:58 -0500

    Notes on 3 attacks

--- RSA/dorothy.h
+++ RSA/dorothy.h
@@ -1,1 +1,14 @@
-//hi
+/*
+  Plan on implementing
+  
+  -If Modulus n is small (less than 512 bits), 
+  then a desktop PC and a good factoring alg
+  can factor it in ONLY a matter of days!
+  
+  -If either p-1 or q-1 have only small prime factors,
+  then n can be factored using Pollard's p-1 alg
+  
+  -If the same message is encrypted using the same public
+  exponent, but different publi moduli, then the message
+  can be recovered using the Chinese remainder theorem
+*/
