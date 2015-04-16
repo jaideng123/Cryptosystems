25c4e17918d409f12f713a163992fbc5bdbc05fd
gsgaytan <gsgaytan@tamu.edu>
2015-04-16 14:50:22 -0500

    Update dorothy.h

--- RSA/dorothy.h
+++ RSA/dorothy.h
@@ -8,7 +8,7 @@
   -If either p-1 or q-1 have only small prime factors,
   then n can be factored using Pollard's p-1 alg
   
-  -If the same message is encrypted using the same public
-  exponent, but different publi moduli, then the message
-  can be recovered using the Chinese remainder theorem
+  -If the public encrytion exponent e is small (ex: e = 3)
+  and the message m is small, so that m^e < n, then m
+  can be recovered by finding the e-th root fo the ciphertext
 */
