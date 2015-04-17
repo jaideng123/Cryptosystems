3329bf47cf3b7dbfd87183297f444e0c912a54b3
jaideng123 <jaideng123@yahoo.com>
2015-04-17 10:30:54 -0500

    Added testcase for prime number gen

--- RSA/RSATestCase.cpp
+++ RSA/RSATestCase.cpp
@@ -1,5 +1,6 @@
 #include <cppunit/config/SourcePrefix.h>
 #include "RSATestCase.h"
+#include "crypt.h"
 
 CPPUNIT_TEST_SUITE_REGISTRATION( RSATestCase );
 
@@ -8,6 +9,12 @@ void RSATestCase::example()
   CPPUNIT_ASSERT_DOUBLES_EQUAL( 1.0, 1.1, 0.05 );
   CPPUNIT_ASSERT( 1 == 1 );
   CPPUNIT_ASSERT( 1 == 1 );
+}
+void RSATestCase::primeTest()
+{
+ 	int value = generate_prime(4);
+ 	CPPUNIT_ASSERT( value == 2 || value == 3 || value == 5 || value == 7 || 
+ 					value == 11|| value == 13);
 }
 void RSATestCase::setUp(){
   m_value1 = 1;
