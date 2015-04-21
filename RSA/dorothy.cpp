9dc02469f3ae143e2af2bf2a3606d8b0c012c5cb
m00sedog00 <m00sedog00@github.tamu.edu>
2015-04-21 17:56:05 -0500

    dorothy getting prepped for RSA attacks

--- RSA/dorothy.cpp
+++ RSA/dorothy.cpp
@@ -1,1 +1,29 @@
-//Star wars trailer is cool
+#include "dorothy.h"
+
+void fermat(){
+	/*TODO:If primes p and q that make up the modulus were created
+  		in a way that makes them likely to be close together,
+  		and therefore close to sq(n), then n can be factored
+  		using Fermat factorization
+	*/
+
+
+}
+
+void pollards(){
+	/*TODO:If either p-1 or q-1 have only small prime factors,
+  		then n can be factored using Pollard's p-1 alg
+  	*/
+
+
+}
+
+void exp_encrypt(){
+
+	/*TODO:If the public encrytion exponent e is small (ex: e = 3)
+  		and the message m is small, so that m^e < n, then m
+  		can be recovered by finding the e-th root fo the ciphertext
+  	*/
+
+
+}
