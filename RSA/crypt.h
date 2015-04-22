a1fcf128c3c9a249863fccaa73a3c814f2e3c5f8
George Gaytan <gsgaytan@tamu.edu>
2015-04-22 16:51:29 -0500

    Update crypt.h

--- RSA/crypt.h
+++ RSA/crypt.h
@@ -5,6 +5,6 @@ BigUnsigned generate_prime(int bit_length);
 BigUnsigned get_randint(int bit_length);
 BigUnsigned pow(BigUnsigned x, BigUnsigned y);
 BigUnsigned modulo(BigUnsigned base, BigUnsigned exponent, BigUnsigned mod);
-bool fermat(BigUnsigned p, int iterations);
+bool fermat_check(BigUnsigned p, int iterations);
 bool miller_rabin(BigUnsigned p,int iterations);
 string decrypt(BigUnsigned c, BigUnsigned d, BigUnsigned n);
