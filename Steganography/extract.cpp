390e19e42d487af70204935de3e9420f1ac850cc
codizzler <crt@riseup.net>
2015-04-25 23:51:53 -0500

    Modified extraction. Sample pairs analysis skeleton uploaded.

--- Steganography/extract.cpp
+++ Steganography/extract.cpp
@@ -23,13 +23,41 @@ ostream& operator<<(ostream& os, const Pixel& pix) {
 	return os;
 }
 
+// Basic sample pairs analysis for image Steganography attack
+bool samplePairsAnalysis(string imageOne, string imageTwo) {
+	cimg_library::CImg<unsigned int>* firstImage = new cimg_library::CImg<unsigned int>(imageOne.c_str());
+	cimg_library::CImg<unsigned int>* secondImage = new cimg_library::CImg<unsigned int>(imageTwo.c_str());
+	vector<bool> matches;
+	for(int i = 0; i < image->width(); ++i) {
+		for(int j = 0; j < image->height(); ++j) {
+			unsigned int r_one = firstImage->atXY(i,j,0);
+			unsigned int g_one = firstImage->atXY(i,j,1);
+			unsigned int b_one = firstImage->atXY(i,j,2);
+			unsigned int r_two = secondImage->atXY(i,j,0);
+			unsigned int g_two = secondImage->atXY(i,j,1);
+			unsigned int b_two = secondImage->atXY(i,j,2);
+			r_one = r_one & 1;
+			g_one = g_one & 1;
+			b_one = b_one & 1;
+			r_two = r_two & 1;
+			g_two = g_two & 1;
+			b_two = b_two & 1;			
+			if (r_one != r_two || g_one != g_two || b_one != b_two)
+				matches.push_back(false);
+			else matches.push_back(true);
+		}
+		extractedData.push_back(row);
+	}
+}
+
 // This function takes the name of an image to extract the LSB plane.
 // The second argument accepts either true for 1-LSB plane or 
 // false for 2-LSB plane
-vector<vector<Pixel> > extractLSB(string imageName, bool one = true) {
+vector<vector<Pixel> > extractLSB(string imageName) {
 
 	cimg_library::CImg<unsigned int>* image = new cimg_library::CImg<unsigned int>(imageName.c_str());
 	vector<vector<Pixel> > extractedData;
+	vector<unsigned int> bits;
 	for(int i = 0; i < image->width(); ++i) {
 		vector<Pixel> row;
 		for(int j = 0; j < image->height(); ++j) {
@@ -37,16 +65,11 @@ vector<vector<Pixel> > extractLSB(string imageName, bool one = true) {
 			unsigned int g = image->atXY(i,j,1);
 			unsigned int b = image->atXY(i,j,2);
 			cout << '(' << r << ',' << g << ',' << b << ")   ";
-			if (one) {
-				r = r & 1;
-				g = g & 1;
-				b = b & 1;
-			} else {
-				r = r & 3;
-				g = g & 3;
-				b = b & 3;
-			}
+			r = r & 1;
+			g = g & 1;
+			b = b & 1;
 			row.push_back(Pixel(r,g,b));
+			bits.push_back(r);
 		}
 		extractedData.push_back(row);
 	}
