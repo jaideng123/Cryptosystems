f91664fc053d6c892ece37d6d1e094916458c091
codizzler <crt@riseup.net>
2015-05-01 19:42:52 -0500

    Fixing compilation errors.

--- StegEmb/DetectionAttack.cpp
+++ StegEmb/DetectionAttack.cpp
@@ -1,28 +1,7 @@
-// Compile command: g++ -std=c++11 DetectionAttack.cpp -I/usr/include/QtGui -lQtGui
-#include <QtGui>
-#include <vector>
-#include <string>
-#include <iostream>
+#include "RandomizationAttack.h"
 
-using namespace std;
 
-struct Pixel {
-	unsigned int r;
-	unsigned int g;
-	unsigned int b;
-	Pixel(unsigned int rr, unsigned int gg, unsigned int bb) {
-		r = rr;
-		g = gg;
-		b = bb;
-	}
-};
-
-ostream& operator<<(ostream& os, const Pixel& pix) {
-	os << '(' << pix.r << ',' << pix.g << ',' << pix.b << ')';
-	return os;
-}
-
-void lowerCase(string& str) {
+void RandomizationAttack::lowerCase(string& str) {
 	for (int i = 0; i < str.length(); ++i) {
 		if (str[i] >= 65 && str[i] <= 90) {
 			str[i] = static_cast<char>(str[i] + ' ');
@@ -33,7 +12,7 @@ void lowerCase(string& str) {
 // Basic detection attack. First argument takes name of image to attck. Second
 // argument takes original image name, or original image md5 as a string
 // Returns true if the image contains hidden data
-bool detectionAttack(string imageOneName, string original) {
+bool RandomizationAttack::detectionAttack(string imageOneName, string original) {
 	bool md_comp = false;
 	if (original.find_first_not_of("0123456789ABCDEFabcdef") == -1 && original.length() == 32) {
 		md_comp = true;
@@ -58,12 +37,4 @@ bool detectionAttack(string imageOneName, string original) {
 		if (imageOne_md == imageTwo_md) return false;
 	}
 	return true;
-}
-
-
-int main() {
-	if (detectionAttack("lena512.bmp","TotallyNotSecretMessage.bmp"))
-		cout << "PICTURE HAS SECRETS!" << endl;
-	else cout << "NO PICTURE SECRETS!" << endl;
-	return 0;
 }
