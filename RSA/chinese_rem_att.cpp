b044ae978c83fcfadd5f6dfcda7749bcf72c4d96
George Gaytan <gsgaytan@tamu.edu>
2015-05-02 16:39:33 -0500

    chinese_rem_att.cpp skeleton

---
+++ RSA/chinese_rem_att.cpp
@@ -0,0 +1,11 @@
+#include "chinese_rem_att.h"
+
+void chinese_rem_att::_chinese_rem(mpf_t n){
+    /*TODO: If the same message is encryoted using the same public exponent,
+      but different public moduli, then the message can be recovered 
+      using the Chinese remainder theorem
+    */
+    
+    
+
+}
