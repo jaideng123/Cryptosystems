6933453fcf716c7b609647b1ecf245ecdc576bc8
jaideng123 <jaideng123@yahoo.com>
2015-05-03 22:51:07 -0500

    munchkincrpyt completely refactored and less brittle

--- RSA/RSA.cpp
+++ RSA/RSA.cpp
@@ -7,18 +7,92 @@
 #include "rsa_keygen.h"
 #include <string.h>
 #include <fstream>
-void interactive(){
-	return;
+string get_nkey(string keyfile){
+	string key;
+	if(keyfile == ""){
+		cout<<"Please Enter N (in base 64):\n>";
+		cin>>key;
+	}
+	else{
+		ifstream myfile;
+		myfile.open(keyfile);
+		if (myfile.is_open())
+		{
+			string line;
+			while ( getline (myfile,line) )
+			{
+				if(line.substr(0,3) == "N: ")
+					key = line.substr(3);
+			}
+			myfile.close();
+		}
+		else{
+			cout<<"Error Opening File! \n";
+			return "";
+		}
+	}
+	return key;
+}
+string  get_private_key(string keyfile){
+	string key;
+	if(keyfile == ""){
+		cout<<"Please Enter Private Key (in base 64):\n>";
+		cin>>key;
+	}
+	else{
+		ifstream myfile;
+		myfile.open(keyfile);
+		if (myfile.is_open())
+		{
+			string line;
+			while ( getline (myfile,line) )
+			{
+				if(line.substr(0,13) == "Private Key: ")
+					key = line.substr(13);
+			}
+			myfile.close();
+		}
+		else{
+			cout<<"Error Opening File! \n";
+			return "";
+		}
+	}
+	return key;
+}
+string  get_public_key(string keyfile){
+	string key;
+	if(keyfile == ""){
+		cout<<"Please Enter Public Key (in base 64):\n>";
+		cin>>key;
+	}
+	else{
+		ifstream myfile;
+		myfile.open(keyfile);
+		if (myfile.is_open())
+		{
+			string line;
+			while ( getline (myfile,line) )
+			{
+				if(line.substr(0,12) == "Public Key: ")
+					key = line.substr(12);
+			}
+			myfile.close();
+		}
+		else{
+			cout<<"Error Opening File! \n";
+			return "";
+		}
+	}
+	return key;
 }
 //handles encoding from command line args
-void encoding(int argc, char* argv[]){
-	BigUnsigned e = from_base_64(argv[2]);
-	BigUnsigned n = from_base_64(argv[3]);
-	if(!strcmp(argv[4], "-f")){
-		string filename = argv[5];
+void encoding(string infile,string outfile,string keyfile){
+	BigUnsigned e = from_base_64(get_public_key(keyfile).c_str());
+	BigUnsigned n = from_base_64(get_nkey(keyfile).c_str());
+	string cleartext;
+	if(infile != ""){
 		ifstream myfile;
-		myfile.open(filename);
-		string cleartext;
+		myfile.open(infile);
 		if (myfile.is_open())
 		{
 			string line;
@@ -32,54 +106,37 @@ void encoding(int argc, char* argv[]){
 			cout<<"Error Opening File! \n";
 			return;
 		}
-		string enc = encrypt_blocks(cleartext,e,n);
-		if(!strcmp(argv[6], "-o")){
-			cout<<"Here is your ciphertext: \n"<<enc;
+	}
+	else{
+		cout<<"Please Input the clear text: \n";
+		getline(cin,cleartext);
+	}
+	string enc = encrypt_blocks(cleartext,e,n);
+	if(outfile != ""){
+		ofstream myfile (outfile);
+		if (myfile.is_open())
+		{
+			myfile << enc;
+			myfile.close();
 		}
-		else if(argc > 7 && !strcmp(argv[6], "-f")){
-			ofstream myfile (argv[7]);
-			if (myfile.is_open())
-			{
-				myfile << enc;
-				myfile.close();
-			}
-			else{
-				cout<<"Error Opening File! \n";
-				return;
-			}
+		else{
+			cout<<"Error Opening File! \n";
+			return;
 		}
 	}
-	else if(!strcmp(argv[4], "-i")){
-		cout<<"Please Input the clear text: \n";
-		string get;
-		getline(cin,get);
-		string enc = encrypt_blocks(get,e,n);
-		if(!strcmp(argv[5], "-o")){
-			cout<<"Here is your ciphertext: \n"<<enc;
-		}
-		else if(argc > 6 && !strcmp(argv[5], "-f")){
-			ofstream myfile (argv[6]);
-			if (myfile.is_open())
-			{
-				myfile << enc;
-				myfile.close();
-			}
-			else{
-				cout<<"Error Opening File! \n";
-				return;
-			}
-		}
+	else{
+		cout<<"Here is your ciphertext: \n"<<enc;
 	}
 }
+
 //handles decoding from command line args
-void decoding(int argc, char* argv[]){
-	BigUnsigned d = from_base_64(argv[2]);
-	BigUnsigned n = from_base_64(argv[3]);
-	if(!strcmp(argv[4], "-f")){
-		string filename = argv[5];
+void decoding(string infile,string outfile,string keyfile){
+	BigUnsigned d = from_base_64(get_private_key(keyfile).c_str());
+	BigUnsigned n = from_base_64(get_nkey(keyfile).c_str());
+	string ciphertext;
+	if(infile != ""){
 		ifstream myfile;
-		myfile.open(filename);
-		string ciphertext;
+		myfile.open(infile);
 		if (myfile.is_open())
 		{
 			string line;
@@ -93,82 +150,78 @@ void decoding(int argc, char* argv[]){
 			cout<<"Error Opening File! \n";
 			return;
 		}
-		string dec = decrypt_blocks(d,n,ciphertext);
-		if(!strcmp(argv[6], "-o")){
-			cout<<"Here is your cleartext: \n";
-			for(char c : dec)
-				cout<<c;
+	}
+	else{
+		cout<<"Please Input the cipher text: \n";
+		getline(cin,ciphertext);
+	}
+	string dec = decrypt_blocks(d,n,ciphertext);
+	if(outfile != ""){
+		ofstream myfile (outfile);
+		if (myfile.is_open())
+		{
+			myfile << dec;
+			myfile.close();
 		}
-		else if(argc > 7 && !strcmp(argv[6], "-f")){
-			ofstream myfile (argv[7]);
-			if (myfile.is_open())
-			{
-				myfile << dec;
-				myfile.close();
-			}
-			else{
-				cout<<"Error Opening File! \n";
-				return;
-			}
+		else{
+			cout<<"Error Opening File! \n";
+			return;
 		}
 	}
-	else if(!strcmp(argv[4], "-i")){
-		cout<<"Please Input the cipher text: \n";
-		string get;
-		getline(cin,get);
-		string enc = decrypt_blocks(d,n,get);
-		if(!strcmp(argv[5], "-o")){
-			cout<<"Here is your clear text: \n"<<enc;
-		}
-		else if(argc > 6 && !strcmp(argv[5], "-f")){
-			ofstream myfile (argv[6]);
-			if (myfile.is_open())
-			{
-				myfile << dec;
-				myfile.close();
-			}
-			else{
-				cout<<"Error Opening File! \n";
-				return;
-			}
-		}
+	else{
+		cout<<"Here is your cleartext: \n"<<dec;
 	}
 	return;
 }
-//enc/dec/genkey e/d n -i/-f filename -o/-f filename
+
 int main(int argc, char* argv[]){
 	if(argc == 1){
 		cout<<"Please Follow the format as follows :\n";
-		cout<<"./munchkicrypt enc/dec <e/d> <n> -i/-f <filename> -o/-f <filename>\n";
+		cout<<"./munchkicrypt enc/dec -i <filename> -o <filename>\n";
 		cout<<"./munchkicrypt genkey -f <keylength>";
 	}
 	if(argc > 1){
-		if(!strcmp(argv[1], "enc") && argc > 5){
-			encoding(argc,argv);
-		}
-		else if(!strcmp(argv[1], "dec") && argc > 5){
-			decoding(argc,argv);
-		}
-		else if(!strcmp(argv[1], "genkey")){
-			int size;
-			string filename;
-			if(argc < 3)
-				size = 16;
-			else{
-				size = atoi(argv[2]);
-				if(argc == 5 && !strcmp(argv[3], "-f") ){
-					filename = argv[4];
-				}
+		string function = argv[1];
+		string infile;
+		string outfile;
+		string keyfile;
+		int size = 16;
+		char c;
+		while ((c = getopt (argc, argv, "i:o:k:s:")) != -1) {
+		  switch(c) {
+			case 'i':
+				infile = optarg;
+				break;
+			case 'o':
+				outfile = optarg;
+				break;
+			case 'k':
+				keyfile = optarg;
+				break;
+			case 's':
+				size = atoi(optarg);
+				break;
+			default:
+				abort();
 			}
+		}
+		if(function == "enc"){
+			encoding(infile,outfile,keyfile);
+		}
+		else if(function == "dec"){
+			decoding(infile,outfile,keyfile);
+		}
+		else if(function == "genkey"){
+			cout<<"Generating key...\n";
 			vector<BigUnsigned> result = generate_keys(generate_prime(size/2), generate_prime(size/2),0);
-			if(filename == ""){
+			if(outfile == ""){
 				cout<<"Public Key: "<<to_base_64(result[2])<<endl;
 				cout<<"Private Key: "<<to_base_64(result[1])<<endl;
 				cout<<"N: "<<to_base_64(result[0])<<endl;
 			}
 			else{
 				ofstream myfile;
-				myfile.open (filename+".key", ios::trunc); 
+				myfile.open (outfile+".key", ios::trunc); 
 				myfile<<"Public Key: "<<to_base_64(result[2])<<endl;
 				myfile<<"Private Key: "<<to_base_64(result[1])<<endl;
 				myfile<<"N: "<<to_base_64(result[0])<<endl;
